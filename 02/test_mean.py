6766356cb5867b0e605e1e4d3a02ba2646331021
IlyaKasiutin <boxfighterilya@gmail.com>
2023-10-01 22:51:30 +0300

    Tests for decorator (unfinished)

---
+++ 02/test_mean.py
@@ -0,0 +1,51 @@
+import unittest
+from unittest import mock
+from mean import mean
+import time
+from io import StringIO
+from contextlib import redirect_stdout
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.func = mock.Mock()
+
+    def test_calls_count(self):
+        @mean(10)
+        def foo(func, *args):
+            func(*args)
+
+        for i in range(20):
+            foo(self.func, 'test string', 12.5)
+
+        self.assertEqual(20, self.func.call_count)
+
+    def test_calls_arguments(self):
+        @mean(10)
+        def foo(func, *args):
+            func(*args)
+
+        for i in range(5):
+            foo(self.func, 'test string', 12.5)
+        expected_calls = [mock.call('test string', 12.5)] * 5
+        self.assertEqual(expected_calls, self.func.mock_calls)
+
+    def test_calculated_value(self):
+        @mean(10)
+        def timer():
+            time.sleep(0.2)
+
+        with StringIO() as buf, redirect_stdout(buf):
+            for i in range(10):
+                timer()
+            output = buf.getvalue()
+            output = float(output.split()[-1])
+            eps = 10e-2
+            self.assertLess(abs(0.2 - output), eps)
+
+    #def test_with_wrong_argument(self):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
