ff1f1d6b94a88b969dbb4839da55cc8755c1ed6a
IlyaKasiutin <boxfighterilya@gmail.com>
2023-10-01 19:36:10 +0300

    Added tests with multiple filters and textio type

--- 01/test_reader.py
+++ 01/test_reader.py
@@ -1,7 +1,5 @@
 import unittest
 from unittest import mock
-import sys
-sys.path.append("../src")
 from file_reader import file_reader
 
 
@@ -29,9 +27,7 @@ class MyReader(unittest.TestCase):
             self.assertEqual(["А роза упала на лапу Азора", "Роза"], lst)
 
     def test_empty_answer(self):
-        text = """
-Roses
-                """
+        text = """Roses"""
         test_file = mock.mock_open(read_data=text)
         with mock.patch('builtins.open', test_file, create=True) as mock_file:
             gen = file_reader("test", ["роза"])
@@ -39,10 +35,18 @@ Roses
             self.assertEqual([], lst)
 
     def test_big_file(self):
-        gen1 = list(file_reader('alice_in_wonderland.txt', 'alice'))
-        gen2 = list(file_reader('alice_in_wonderland.txt', 'ALICE'))
+        gen1 = list(file_reader('alice_in_wonderland.txt', ['alice']))
+        gen2 = list(file_reader('alice_in_wonderland.txt', ['ALICE']))
         self.assertGreater(len(gen1), 0)
         self.assertEqual(list(gen1), list(gen2))
+
+    def test_big_textio(self):
+        with open('alice_in_wonderland.txt') as file:
+            gen1 = list(file_reader(file, ['alice']))
+            file.seek(0)
+            gen2 = list(file_reader(file, ['ALICE']))
+            self.assertGreater(len(gen1), 0)
+            self.assertEqual(list(gen1), list(gen2))
 
     def test_multiple_words(self):
         text = """
@@ -55,6 +59,16 @@ Roses
             gen = file_reader("test", ["первая", "вторая"])
             lst = list(gen)
             self.assertEqual(["Первая строка", "Вторая строка"], lst)
+
+    def test_multiple_filters_in_one_string(self):
+        text = """Первая строка текста
+Вторая строка
+Третья строка"""
+        test_file = mock.mock_open(read_data=text)
+        with mock.patch('builtins.open', test_file, create=True) as mock_file:
+            gen = file_reader("test", ["первая", "вторая", "текста"])
+            lst = list(gen)
+            self.assertEqual(["Первая строка текста", "Вторая строка"], lst)
 
 
 if __name__ == '__main__':
