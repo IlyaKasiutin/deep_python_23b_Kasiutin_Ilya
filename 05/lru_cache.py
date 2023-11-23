eddd05864705212f2cf10a3461e766151c600449
IlyaKasiutin <boxfighterilya@gmail.com>
2023-11-23 16:13:13 +0300

    New LRUCache implementation

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -1,26 +1,73 @@
 class LRUCache:
     def __init__(self, limit=42):
         self.limit = limit
-        self.values = {}
-        self.order = []
-
-    def __update_order(self, key):
-        if key in self.order:
-            self.order.remove(key)
-        self.order.append(key)
-
-        if len(self.order) > self.limit:
-            deleted_key = self.order.pop(0)
-            del self.values[deleted_key]
+        self.cache = {}
+        self.head = None
+        self.tail = None
 
     def get(self, key):
-        if key not in self.values:
+        if key not in self.cache:
             return None
 
-        val = self.values[key]
-        self.__update_order(key)
-        return val
+        node = self.cache[key]
+        self.__move_to_front(node)
+        return node.value
 
     def set(self, key, value):
-        self.values[key] = value
-        self.__update_order(key)
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self.__move_to_front(node)
+        else:
+            node = Node(key, value)
+            self.cache[key] = node
+            self.__add_to_front(node)
+
+            if len(self.cache) > self.limit:
+                self.__remove_last()
+
+    def __add_to_front(self, node):
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+
+    def __remove_last(self):
+        if not self.tail:
+            return
+
+        del self.cache[self.tail.key]
+
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+
+    def __move_to_front(self, node):
+        if node == self.head:
+            return
+
+        if node == self.tail:
+            self.tail = node.prev
+            self.tail.next = None
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+
+        node.prev = None
+        node.next = self.head
+        self.head.prev = node
+        self.head = node
+
+
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
