d50c08c0b5c12592015b345d82c7b8163e37dddf
IlyaKasiutin <boxfighterilya@gmail.com>
2023-11-23 19:25:38 +0300

    add arg -s handling

--- 09/lru_cache.py
+++ 09/lru_cache.py
@@ -1,47 +1,46 @@
 import logging
 
 
+logger = logging.getLogger("LRUCache")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler("cache.log", 'w')
+formatter = logging.Formatter("%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
 class LRUCache:
-    logger = logging.getLogger("LRUCache")
-    logger.setLevel(logging.DEBUG)
-
-    handler = logging.FileHandler("cache.log", 'w')
-    formatter = logging.Formatter("%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s")
-    handler.setFormatter(formatter)
-
-    logger.addHandler(handler)
-
     def __init__(self, limit=42):
         self.limit = limit
         self.cache = {}
         self.head = None
         self.tail = None
-        self.logger.debug("New LRUCache instance created")
+        logger.debug("New LRUCache instance created")
 
     def get(self, key):
         if key not in self.cache:
-            self.logger.warning("Got key='%s' not in cache", key)
+            logger.info("Got key='%s' which is not in cache", key)
             return None
 
         node = self.cache[key]
         self.__move_to_front(node)
-        self.logger.debug("Got key='%s' from cache", key)
+        logger.debug("Got key='%s' from cache", key)
         return node.value
 
     def set(self, key, value):
         if key in self.cache:
             node = self.cache[key]
             node.value = value
-            self.logger.debug("Updated key='%s'", key)
+            logger.debug("Updated key='%s'", key)
             self.__move_to_front(node)
         else:
             node = Node(key, value)
             self.cache[key] = node
-            self.logger.debug("Added key='%s' to cache", key)
+            logger.debug("Added key='%s' to cache", key)
             self.__add_to_front(node)
 
             if len(self.cache) > self.limit:
-                self.logger.warning("Removing last key...")
+                logger.info("Removing last key...")
                 self.__remove_last()
 
     def __add_to_front(self, node):
@@ -59,7 +58,7 @@ class LRUCache:
 
         last_key = self.tail.key
         last_elem = self.cache.pop(self.tail.key).value
-        self.logger.debug("Removed key='%s', value='%s'", last_key, last_elem)
+        logger.debug("Removed key='%s', value='%s'", last_key, last_elem)
 
         if self.head == self.tail:
             self.head = None
@@ -84,23 +83,14 @@ class LRUCache:
         self.head.prev = node
         self.head = node
 
-        self.logger.info("Moved to front key='%s', value='%s'", self.head.key, self.head.value)
+        logger.info("Moved to front key='%s', value='%s'", self.head.key, self.head.value)
 
 
 class Node:
-    logger = logging.getLogger("Node")
-    logger.setLevel(logging.DEBUG)
-
-    handler = logging.FileHandler("cache.log", 'w')
-    formatter = logging.Formatter("%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s")
-    handler.setFormatter(formatter)
-
-    logger.addHandler(handler)
-
     def __init__(self, key, value):
         self.key = key
         self.value = value
         self.prev = None
         self.next = None
 
-        self.logger.debug("New Node instance created")
+        logger.debug("New Node instance created")
--- 09/main.py
+++ 09/main.py
@@ -1,10 +1,18 @@
 import argparse
-from lru_cache import LRUCache
+import logging
+from lru_cache import LRUCache, logger
 
 
 parser = argparse.ArgumentParser('logger')
 parser.add_argument('-s', action='store_true')
 parser.add_argument('-f', action='store_true')
+args = parser.parse_args()
+
+if args.s:
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("stdout %(asctime)s\t%(levelname)s\t%(name)s\t%(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 
 cache = LRUCache(2)
@@ -12,6 +20,5 @@ cache = LRUCache(2)
 cache.set("k1", "val1")
 cache.set("k2", "val2")
 cache.set("k3", "val3")
-
-
-
+cache.get("k1")
+cache.get("k2")
