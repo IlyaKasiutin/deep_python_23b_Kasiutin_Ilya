e070e9776156336d334a702623d50c536d6af602
IlyaKasiutin <boxfighterilya@gmail.com>
2023-11-28 09:03:03 +0300

    Refactor - print_stats

--- 08/task3/profile_deco.py
+++ 08/task3/profile_deco.py
@@ -1,26 +1,10 @@
 """Profile decorator"""
 import cProfile
-from typing import Callable
-
-
-class ProfiledFunc:
-    """Class for print_stat implementation"""
-    def __init__(self, func: Callable, profiler_stats: cProfile.Profile):
-        self.func = func
-        self.profiler_stats = profiler_stats
-
-    def print_stat(self):
-        """For profiling output"""
-        print(f"Stats for {self.func.__name__}")
-        print('-' * 50)
-        self.profiler_stats.print_stats(sort='ncalls')
-        print('-' * 50)
 
 
 def profile_deco(func):
     """Function calls profiling decorator"""
     profiler = cProfile.Profile()
-    profiled_function = ProfiledFunc(func, profiler)
 
     def wrapper(*args, **kwargs):
         profiler.enable()
@@ -28,6 +12,6 @@ def profile_deco(func):
         profiler.disable()
         return res
 
-    wrapper.print_stat = profiled_function.print_stat
+    wrapper.print_stat = profiler.print_stats
 
     return wrapper
