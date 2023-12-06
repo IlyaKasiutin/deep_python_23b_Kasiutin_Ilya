80dfa6c6e74007982af71c8bd8a91a4a6db63bc0
IlyaKasiutin <boxfighterilya@gmail.com>
2023-12-06 10:47:07 +0300

    Refactor - logging config in functions

--- 09/logging_config.py
+++ 09/logging_config.py
@@ -13,6 +13,14 @@ class CustomFilter(logging.Filter):
         return 'key' in record.msg
 
 
+def get_filters(args):
+    filters = []
+    if args.f:
+        custom_filter = CustomFilter()
+        filters.append(custom_filter)
+    return filters
+
+
 def get_file_handler(filename: str, mode='w'):
     handler = logging.FileHandler(filename, mode)
     handler.setFormatter(FILE_FORMATTER)
@@ -25,7 +33,35 @@ def get_stream_handler():
     return handler
 
 
+def get_handlers(args):
+    handlers = []
+    file_handler = get_file_handler("cache.log")
+    handlers.append(file_handler)
+
+    if args.s:
+        stream_handler = get_stream_handler()
+        handlers.append(stream_handler)
+    return handlers
+
+
 def create_custom_logger(name: str, level: int):
     logger = logging.getLogger(name)
     logger.setLevel(level)
     return logger
+
+
+def apply_filters_to_handlers(handlers: list[logging.Handler], filters: list[logging.Filter]):
+    if not filters:
+        return
+
+    for handler in handlers:
+        for f in filters:
+            handler.addFilter(f)
+
+
+def apply_handlers_to_logger(logger: logging.Logger, handlers: list[logging.Handler]):
+    if not handlers:
+        return
+
+    for handler in handlers:
+        logger.addHandler(handler)
--- 09/main.py
+++ 09/main.py
@@ -3,30 +3,17 @@
 
 from parsing_args import get_args
 from lru_cache import LRUCache, logger
-from logging_config import CustomFilter, get_file_handler, get_stream_handler
-
-
-args = get_args()
-
-
-file_handler = get_file_handler("cache.log")
-handlers = [file_handler]
-logger.addHandler(file_handler)
-
-if args.s:
-    stream_handler = get_stream_handler()
-    handlers.append(stream_handler)
-
-if args.f:
-    custom_filter = CustomFilter()
-    for handler in handlers:
-        handler.addFilter(custom_filter)
-
-for handler in handlers:
-    logger.addHandler(handler)
+from logging_config import get_handlers, get_filters, apply_filters_to_handlers, apply_handlers_to_logger
 
 
 if __name__ == "__main__":
+    args = get_args()
+
+    handlers = get_handlers(args)
+    filters = get_filters(args)
+    apply_filters_to_handlers(handlers, filters)
+    apply_handlers_to_logger(logger, handlers)
+
     cache = LRUCache(2)
 
     cache.set("k1", "val1")
